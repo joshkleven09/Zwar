b064f865e7bca1c89e8eb5a5f0cc2ff270630f25
Craig Martin <cmgecola@gmail.com>
2015-02-11 20:19:35 -0600

    Clamp circle to camera view

--- Assets/CircleController.cs
+++ Assets/CircleController.cs
@@ -9,11 +9,18 @@ public class CircleController : MonoBehaviour {
 	public float speed = 10;             // Movement speed
 	public bool useKeyboardInput = true;   // Use Keyboard input or Joystick
 	private float h = 0, v = 0;         // Horizontal and Vertical values
-	
-	
+
+	private Rect cameraRect;
+
 	// Use this for initialization
 	void Start () {
-	
+		Vector3 bottomLeft = Camera.main.ViewportToWorldPoint (Vector3.zero);
+		Vector3 topRight = Camera.main.ViewportToWorldPoint (Vector3.one);
+
+		cameraRect = new Rect (bottomLeft.x + renderer.bounds.extents.x,
+		                       bottomLeft.y + renderer.bounds.extents.y,
+		                       topRight.x - bottomLeft.x - (2 * renderer.bounds.extents.x),
+		                       topRight.y - bottomLeft.y - (2 * renderer.bounds.extents.y));
 	}
 	
 	// Update is called once per frame
@@ -40,7 +47,6 @@ public class CircleController : MonoBehaviour {
 			v = Input.GetAxis("Vertical");
 		}
 
-
 		// Apply horizontal velocity
 		if (Mathf.Abs(h) > 0) {
 			transform.rigidbody2D.velocity = new Vector3(h * speed, transform.rigidbody2D.velocity.y);
@@ -56,8 +62,9 @@ public class CircleController : MonoBehaviour {
 		else{
 			transform.rigidbody2D.velocity = new Vector3(transform.rigidbody2D.velocity.x, 0);
 		}
-		
+
+		// Clamp the circle within the view of the camera
+		transform.rigidbody2D.position = new Vector2(Mathf.Clamp (transform.rigidbody2D.position.x, cameraRect.xMin, cameraRect.xMax),
+		                                             Mathf.Clamp (transform.rigidbody2D.position.y, cameraRect.yMin, cameraRect.yMax));
 	}
-
-
 }
